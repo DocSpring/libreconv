f5b52fc5b05d7f666d524d8a5e936f9b5a787ee6
Richard Nyström <ricny046@gmail.com>
2013-03-24 23:07:55 +0100

    Two specs passing

--- lib/libreconv.rb
+++ lib/libreconv.rb
@@ -1,5 +1,37 @@
 require "libreconv/version"
 
 module Libreconv
-  # Your code goes here...
+
+  def self.convert(source, soffice_command = nil)
+    
+    unless soffice_present?(soffice_command) 
+      raise IOError, "Can't find Libreoffice or Openoffice executable."
+    end
+
+    unless File.exists?(source)
+      raise IOError, "Source file (#{@source}) does not exist."
+    end
+  end
+
+  def self.which(cmd)
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each { |ext|
+        exe = File.join(path, "#{cmd}#{ext}")
+        return exe if File.executable? exe
+      }
+    end
+    
+    return nil
+  end
+
+  private
+
+  def self.soffice_present?(soffice_command = nil)
+    if soffice_command
+      File.exists?(soffice_command)
+    else
+      self.which("soffice.exe") || self.which("soffice.bin") || self.which("soffice")
+    end
+  end
 end
--- spec/libreconv_spec.rb
+++ spec/libreconv_spec.rb
@@ -5,20 +5,20 @@ require 'spec_helper'
 describe Libreconv do 
   
   before(:all) do
-    @docx_parser = Rika::Parser.new(file_path("document.docx"))
+    @docx_file = file_path("document.docx")
+    @doc_file = file_path("document.doc")
   end
 
   after(:all) do
   end
 
   describe '.convert' do
-    it "should raise error if soffice or soffice.bin command does not exists" do
-      lambda { Libreconv.convert(file_path("nonsense.txt")) }.should raise_error(IOError)
+    it "should raise error if soffice command does not exists" do
+      lambda { Libreconv.convert(@doc_file, "/Whatever/soffice") }.should raise_error(IOError)
     end
 
     it "should raise error if source file does not exists" do
-      pending
+      lambda { Libreconv.convert(file_path("nonsense.txt")) }.should raise_error(IOError)
     end
-    
   end
 end
