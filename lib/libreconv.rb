2221065b850ce11223d0053b226630967d18e62d
Richard Nyström <ricny046@gmail.com>
2013-03-25 00:28:58 +0100

    Use proper block syntax

--- lib/libreconv.rb
+++ lib/libreconv.rb
@@ -41,10 +41,10 @@ module Libreconv
     def which(cmd)
       exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
       ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exts.each { |ext|
+        exts.each do |ext|
           exe = File.join(path, "#{cmd}#{ext}")
           return exe if File.executable? exe
-        }
+        end
       end
     
       return nil
