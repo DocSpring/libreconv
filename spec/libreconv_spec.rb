e687104038bf8de5b8c9a0bb44175f6d298dac4c
Richard Nyström <ricny046@gmail.com>
2013-03-25 22:58:27 +0100

    Refactored specs

--- spec/libreconv_spec.rb
+++ spec/libreconv_spec.rb
@@ -29,33 +29,29 @@ describe Libreconv do
 
     describe "#convert" do
       it "should convert a doc to pdf" do
-        source = @doc_file
-        target_file = "#{@target_path}/#{File.basename(source, ".doc")}.pdf" 
-        converter = Libreconv::Converter.new(source, @target_path)
+        target_file = "#{@target_path}/#{File.basename(@doc_file, ".doc")}.pdf" 
+        converter = Libreconv::Converter.new(@doc_file, @target_path)
         converter.convert
         File.exists?(target_file).should == true
       end
 
       it "should convert a docx to pdf" do
-        source = @docx_file
-        target_file = "#{@target_path}/#{File.basename(source, ".docx")}.pdf" 
-        converter = Libreconv::Converter.new(source, @target_path)
+        target_file = "#{@target_path}/#{File.basename(@docx_file, ".docx")}.pdf" 
+        converter = Libreconv::Converter.new(@docx_file, @target_path)
         converter.convert
         File.exists?(target_file).should == true
       end
 
       it "should convert a pptx to pdf" do
-        source = @pptx_file
-        target_file = "#{@target_path}/#{File.basename(source, ".pptx")}.pdf" 
-        converter = Libreconv::Converter.new(source, @target_path)
+        target_file = "#{@target_path}/#{File.basename(@pptx_file, ".pptx")}.pdf" 
+        converter = Libreconv::Converter.new(@pptx_file, @target_path)
         converter.convert
         File.exists?(target_file).should == true
       end
 
       it "should convert a ppt to pdf" do
-        source = @ppt_file
-        target_file = "#{@target_path}/#{File.basename(source, ".ppt")}.pdf" 
-        converter = Libreconv::Converter.new(source, @target_path)
+        target_file = "#{@target_path}/#{File.basename(@ppt_file, ".ppt")}.pdf" 
+        converter = Libreconv::Converter.new(@ppt_file, @target_path)
         converter.convert
         File.exists?(target_file).should == true
       end
