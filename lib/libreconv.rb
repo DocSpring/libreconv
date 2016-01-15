ed555306340af7756fffda4e356c3b0e6ee448e4
Danesh Dadachanji <danesh@divnotes.com>
2016-01-15 14:12:34 -0500

    Escape URL when calling soffice and remove GET params when assigning tmp location.

--- lib/libreconv.rb
+++ lib/libreconv.rb
@@ -19,7 +19,7 @@ module Libreconv
       @soffice_command = soffice_command
       @convert_to = convert_to || "pdf"
       determine_soffice_command
-      check_source_type
+      @source_type = check_source_type
 
       unless @soffice_command && File.exists?(@soffice_command)
         raise IOError, "Can't find Libreoffice or Openoffice executable."
@@ -30,10 +30,14 @@ module Libreconv
       orig_stdout = $stdout.clone
       $stdout.reopen File.new('/dev/null', 'w')
       Dir.mktmpdir { |target_path|
-        pid = Spoon.spawnp(@soffice_command, "--headless", "--convert-to", @convert_to, @source, "--outdir", target_path)
+        # If the URL contains GET params, the '&' could break when being used
+        # as an argument to soffice. Wrap it in single quotes to escape it.
+        escaped_source = @source_type == 1 ? @source : "'#{@source}'"
+        pid = Spoon.spawnp(@soffice_command, "--headless", "--convert-to", @convert_to, source, "--outdir", target_path)
         Process.waitpid(pid)
         $stdout.reopen orig_stdout
-        target_tmp_file = "#{target_path}/#{File.basename(@source, ".*")}.#{File.basename(@convert_to, ":*")}"
+        escaped_source_path = @source_type == 1 ? @source : URI.parse(@source).path # Strip any GET params that the URL may have
+        target_tmp_file = "#{target_path}/#{File.basename(escaped_source_path, ".*")}.#{File.basename(@convert_to, ":*")}"
         FileUtils.cp target_tmp_file, @target
       }
     end
@@ -60,9 +64,9 @@ module Libreconv
     end
 
     def check_source_type
-      return if File.exists?(@source) && !File.directory?(@source) #file
-      return if URI(@source).scheme == "http" && Net::HTTP.get_response(URI(@source)).is_a?(Net::HTTPSuccess) #http
-      return if URI(@source).scheme == "https" && Net::HTTP.get_response(URI(@source)).is_a?(Net::HTTPSuccess) #https
+      return 1 if File.exists?(@source) && !File.directory?(@source) #file
+      return 2 if URI(@source).scheme == "http" && Net::HTTP.get_response(URI(@source)).is_a?(Net::HTTPSuccess) #http
+      return 2 if URI(@source).scheme == "https" && Net::HTTP.get_response(URI(@source)).is_a?(Net::HTTPSuccess) #https
       raise IOError, "Source (#{@source}) is neither a file nor an URL."
     end
   end
